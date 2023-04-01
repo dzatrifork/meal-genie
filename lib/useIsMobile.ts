7cf59624e220ccff79b9ff7a35e96dfde1f7ddbf
dzatrifork <dza@trifork.com>
2023-04-01 13:21:15 +0200

    fix window prererendering

--- lib/useIsMobile.ts
+++ lib/useIsMobile.ts
@@ -1,21 +1,21 @@
-import React, {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const useIsMobile = () => {
-    const [width, setWidth] = useState(window.innerWidth ?? 1000);
-    const handleWindowSizeChange = () => {
-            setWidth(window.innerWidth);
+  const [width, setWidth] = useState(window?.innerWidth ?? 1000);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    if (window != null) {
+      window.addEventListener("resize", handleWindowSizeChange);
+      return () => {
+        window.removeEventListener("resize", handleWindowSizeChange);
+      };
     }
+  }, []);
 
-    useEffect(() => {
-        if (window != null) {
-            window.addEventListener('resize', handleWindowSizeChange);
-            return () => {
-                window.removeEventListener('resize', handleWindowSizeChange);
-            }
-        }
-    }, []);
+  return width <= 768;
+};
 
-    return (width <= 768);
-}
-
-export default useIsMobile
+export default useIsMobile;
